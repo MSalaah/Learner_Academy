8fb69867e0c756683bd069263b33807c4a5a8b36
Moe <mohamed.salahhussein@vodafone.com>
2023-03-26 12:17:35 +0200

    initial commit

---
+++ src/main/java/servlets/LoginServlet.java
@@ -0,0 +1,61 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+    	System.out.println("Login Servlet");
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.sendRedirect("login.html");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setHeader("Cache-Control", "no-cache,no-store,must-revalidate");
+		response.setHeader("Pragma", "no-cache");//http1.0
+		response.setHeader("Pragma", "0"); //proxies
+		
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		
+    	System.out.println(email);
+    	System.out.println(password);
+
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+		if(email!=null && email.equals("admin") && password!=null && password.equals("admin"))
+		{
+			HttpSession session = request.getSession();
+			session.setAttribute("id", email);
+			response.sendRedirect("admin");
+			return;
+		}		
+	}
+}
